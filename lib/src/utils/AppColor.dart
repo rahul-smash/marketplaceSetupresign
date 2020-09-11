5d1cfe9c9fc5a94d5a54c95475bd1ba26eca1b9e
apple <apple@apples-MacBook-Pro.local>
2020-09-11 19:25:05 +0530

    worked on category listing screen

--- lib/src/utils/AppColor.dart
+++ lib/src/utils/AppColor.dart
@@ -18,7 +18,7 @@ Color leftMenuUsernameColors = Color(0xffffffff);
 
 Color bottomBarIconColor = Color(0xffffffff);
 Color bottomBarTextColor = Color(0xffffffff);
-
+Color grey2 = Color(0xffF4F6F5);
 
 Color dotIncreasedColor = Color(0xff777A18);
 
@@ -26,6 +26,7 @@ Color dotIncreasedColor = Color(0xff777A18);
 Color bottomBarBackgroundColor = Color(0xffffffff);
 Color gridBackgroundCOlor = Color(0xffECECEC);
 Color orangeColor = Color(0xffFF5400);
+Color yellow = Color(0xffFFAD01);
 Color grayColor = Color(0xffd3d3d3);
 Color favGrayColor = Color(0xfff3f3f3);
 Color searchGrayColor = Color(0xfff0f0f0);
@@ -35,5 +36,11 @@ Color whiteColor = Color(0xffffffff);
 Color toastbgColor = Color(0xff656565);
 Color grayColorTitle = Color(0xff474d54);
 Color grayLightColor = Color(0xFFEEEEEE);
+
+Color lightRed = Color(0xFFFBF1F0);
+
+Color darkRed = Color(0xFFDA1C1B);
+
+
 
 
