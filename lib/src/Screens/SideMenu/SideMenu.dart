f14868b0830dc1d1fae7b98edb4dccfed2af53f0
karan <karan@signitysolutions.in>
2021-01-05 09:43:45 +0530

    updated code

--- lib/src/Screens/SideMenu/SideMenu.dart
+++ lib/src/Screens/SideMenu/SideMenu.dart
@@ -6,6 +6,7 @@ import 'package:flutter_facebook_login/flutter_facebook_login.dart';
 import 'package:flutter_open_whatsapp/flutter_open_whatsapp.dart';
 import 'package:flutter_share/flutter_share.dart';
 import 'package:google_sign_in/google_sign_in.dart';
+import 'package:restroapp/src/Screens/BookOrder/MyCartScreen.dart';
 import 'package:restroapp/src/Screens/Favourites/Favourite.dart';
 import 'package:restroapp/src/Screens/LoginSignUp/LoginMobileScreen.dart';
 import 'package:restroapp/src/Screens/Offers/MyOrderScreenVersion2.dart';
@@ -18,6 +19,7 @@ import 'package:restroapp/src/apihandler/ApiController.dart';
 import 'package:restroapp/src/database/DatabaseHelper.dart';
 import 'package:restroapp/src/database/SharedPrefs.dart';
 import 'package:restroapp/src/models/BrandModel.dart';
+import 'package:restroapp/src/models/CategoryResponseModel.dart';
 import 'package:restroapp/src/models/ReferEarnData.dart';
 import 'package:restroapp/src/models/SocialModel.dart';
 import 'package:restroapp/src/models/UserResponseModel.dart';
@@ -38,8 +40,9 @@ class NavDrawerMenu extends StatefulWidget {
   final BrandData brandData;
   final String userName;
   VoidCallback callback;
+  bool isPWAThemeEnable = true;
 
-  NavDrawerMenu(this.brandData, this.userName,this.callback);
+  NavDrawerMenu(this.brandData, this.userName, this.callback,);
 
   @override
   _NavDrawerMenuState createState() {
@@ -70,7 +73,11 @@ class _NavDrawerMenuState extends State<NavDrawerMenu> {
     _drawerItems.add(DrawerChildItem(
         DrawerChildConstants.MY_PROFILE, "images/myprofile.png"));
     _drawerItems.add(DrawerChildItem(
+        DrawerChildConstants.Categories, "images/unselectedcategoryicon.png"));
+    _drawerItems.add(DrawerChildItem(
         DrawerChildConstants.DELIVERY_ADDRESS, "images/deliveryaddress.png"));
+    _drawerItems
+        .add(DrawerChildItem(DrawerChildConstants.Cart, "images/carticon.png"));
     _drawerItems.add(
         DrawerChildItem(DrawerChildConstants.MY_ORDERS, "images/my_order.png"));
     if (widget.brandData.loyality == "1")
@@ -94,6 +101,15 @@ class _NavDrawerMenuState extends State<NavDrawerMenu> {
     } catch (e) {
       print(e);
     }
+
+    if (widget.isPWAThemeEnable) {
+      left_menu_background_color = Colors.white;
+      left_menu_header_bkground = Colors.white;
+      left_menu_icon_colors = Colors.black;
+      leftMenuLabelTextColors = Colors.black;
+      leftMenuWelcomeTextColors = Colors.black;
+    }
+
   }
 
   @override
@@ -117,41 +133,42 @@ class _NavDrawerMenuState extends State<NavDrawerMenu> {
   Widget createHeaderInfoItem() {
     return Container(
         color: left_menu_header_bkground,
-        child: Padding(
-            padding: EdgeInsets.only(left: 35, top: 40, bottom: 30),
-            child: Row(children: [
+        padding: EdgeInsets.only(top: 40, bottom: 15),
+        child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              /*Image.asset('images/profileimg.png',color: Color(0xFFD6D6D6),*/
+              Icon(
+                Icons.account_circle,
+                size: 100,
+                color: Color(0xFFD6D6D6),
+              ),
               Padding(
-                padding: EdgeInsets.only(left: 0, right: 20),
-                child: Center(
-                  child: Icon(
-                    Icons.account_circle,
-                    size: 60,
-                    color: Colors.white,
-                  ),
-                ),
-              ),
-              Flexible(
-                child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: <Widget>[
-                      Text('Welcome',
-                          style: TextStyle(
-                              color: leftMenuWelcomeTextColors,
-                              fontSize: 18,
-                              fontWeight: FontWeight.bold)),
-                      SizedBox(height: 5),
-                      Text(
-                          AppConstant.isLoggedIn == false
-                              ? ''
-                              : widget.userName,
-                          maxLines: 2,
-                          overflow: TextOverflow.ellipsis,
-                          style: TextStyle(
-                              color: leftMenuWelcomeTextColors, fontSize: 15)),
-                    ]),
-              ),
-            ])));
+                  padding: EdgeInsets.only(top: 10, left: 16, right: 16),
+                  child: Text(
+                      'Welcome' +
+                          '${AppConstant.isLoggedIn == false ? '' : ' ' + widget.userName}',
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                          color: leftMenuWelcomeTextColors,
+                          fontSize: 18,
+                          fontWeight: FontWeight.bold))),
+//          Visibility(
+//              visible: AppConstant.isLoggedIn,
+//              child: Column(
+//                children: [
+//                  SizedBox(height: 5),
+//                  Text(AppConstant.isLoggedIn == false ? '' : widget.userName,
+//                      maxLines: 2,
+//                      overflow: TextOverflow.ellipsis,
+//                      style: TextStyle(
+//                          color: leftMenuWelcomeTextColors, fontSize: 15)),
+//                ],
+//              ))
+            ]));
   }
+
 
   Widget createDrawerItem(int index, BuildContext context) {
     var item = _drawerItems[index];
@@ -206,6 +223,22 @@ class _NavDrawerMenuState extends State<NavDrawerMenu> {
           Utils.showLoginDialog(context);
         }
         break;
+      case DrawerChildConstants.Cart:
+        if (AppConstant.isLoggedIn) {
+          Navigator.pop(context);
+          Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => MyCartScreen(() {}),
+              ));
+          Map<String, dynamic> attributeMap = new Map<String, dynamic>();
+          attributeMap["ScreenName"] = "MyCartScreen";
+          Utils.sendAnalyticsEvent("Clicked MyCartScreen", attributeMap);
+        } else {
+          Utils.showLoginDialog(context);
+        }
+        break;
+
       case DrawerChildConstants.DELIVERY_ADDRESS:
         if (AppConstant.isLoggedIn) {
           Navigator.pop(context);
@@ -219,21 +252,23 @@ class _NavDrawerMenuState extends State<NavDrawerMenu> {
           attributeMap["ScreenName"] = "DeliveryAddressList";
           Utils.sendAnalyticsEvent("Clicked DeliveryAddressList", attributeMap);
         } else {
+          Navigator.pop(context);
           Utils.showLoginDialog(context);
         }
         break;
       case DrawerChildConstants.MY_ORDERS:
         if (AppConstant.isLoggedIn) {
           Navigator.pop(context);
-          /*Navigator.push(
+          Navigator.push(
             context,
             MaterialPageRoute(
-                builder: (context) => MyOrderScreenVersion2(widget.store)),
-          );*/
+                builder: (context) => MyOrderScreenVersion2(widget.brandData)),
+          );
           Map<String, dynamic> attributeMap = new Map<String, dynamic>();
           attributeMap["ScreenName"] = "MyOrderScreen";
           Utils.sendAnalyticsEvent("Clicked MyOrderScreen", attributeMap);
         } else {
+          Navigator.pop(context);
           Utils.showLoginDialog(context);
         }
         break;
@@ -246,6 +281,7 @@ class _NavDrawerMenuState extends State<NavDrawerMenu> {
                 builder: (context) => LoyalityPointsScreen(widget.brandData)),
           );
         } else {
+          Navigator.pop(context);
           Utils.showLoginDialog(context);
         }
 
@@ -375,7 +411,7 @@ class _NavDrawerMenuState extends State<NavDrawerMenu> {
             ),
             FlatButton(
               child: const Text('YES'),
-              onPressed: () {
+              onPressed: () async {
                 Navigator.of(context).pop();
                 logout(context);
               },
@@ -486,11 +522,17 @@ class DrawerChildConstants {
   static const MY_PROFILE = "My Profile";
   static const DELIVERY_ADDRESS = "Delivery Address";
   static const MY_ORDERS = "My Orders";
+  static const Cart = "Cart";
+  static const Categories = "Categories";
   static const LOYALITY_POINTS = "Loyality Points";
   static const MY_FAVORITES = "My Favorites";
   static const ABOUT_US = "About Us";
+  static const ADDITION_INFORMATION = "Addition \nInformation";
   static const SHARE = "Share";
   static const FAQ = "FAQ";
+  static const TERMS_CONDITIONS = "Terms and Conditions";
+  static const PRIVACY_POLICY = "Privacy Policy";
+  static const REFUND_POLICY = "Refund Policy";
   static const ReferEarn = "Refer & Earn";
   static const LOGIN = "Login";
   static const LOGOUT = "Logout";
