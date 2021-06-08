3c8ea389b9b649f21c04f7c7c632fa8cec387f6e
Himanshu Vatsh <himanshu.vatsh@signitysolutions.com>
2021-06-08 11:53:45 +0530

    issues fixed

--- lib/src/Screens/Dashboard/HomeCategoryListView.dart
+++ lib/src/Screens/Dashboard/HomeCategoryListView.dart
@@ -189,7 +189,7 @@ class _HomeCategoryListViewState extends State<HomeCategoryListView> {
   _showAllSubCategories() {
     if (Utils.checkIfStoreClosed(widget.store)) {
       DialogUtils.displayCommonDialog(
-          context, widget.store.storeName, widget.store.storeMsg);
+          context, widget.store.storeName, widget.store.stor6eMsg);
     } else {
       if (widget.selectedCategory != null &&
           widget.selectedCategory.subCategory != null &&
