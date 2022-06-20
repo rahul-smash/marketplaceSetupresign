cc5ad58a0af7d03f46f1829a308837db9e0aabc6
karan <karan@signitysolutions.in>
2022-06-20 15:18:41 +0530

    updated

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,11 +1,10 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/home/signity/flutter_development/flutter"
-export "FLUTTER_APPLICATION_PATH=/home/signity/workspace_signity/1_Flutter_projects/marketplace_setup_2"
+export "FLUTTER_ROOT=/Users/karanpaul/Documents/sdk/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/karanpaul/Documents/valuesAppz/marketplace_setup"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
-export "SYMROOT=${SOURCE_ROOT}/../build/ios"
 export "FLUTTER_BUILD_NAME=1.0.0"
 export "FLUTTER_BUILD_NUMBER=1"
 export "DART_OBFUSCATION=false"
